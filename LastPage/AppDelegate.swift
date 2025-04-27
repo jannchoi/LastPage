01e37690bf1ec119cf62ad0c99288523aac4eb23
jannchoi <gongjo3695@gmail.com>
2025-04-27 21:58:07 +0900

    refactor: firebase Analytics 코드 추가

--- LastPage/AppDelegate.swift
+++ LastPage/AppDelegate.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 import RealmSwift
+import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        FirebaseApp.configure()
         migration()
         
         let realm = try! Realm()
